f8f1fece54d12513b0a2176078ff76c60d17fd6f
Rafael Lisboa <rafaelzlisboa@gmail.com>
2015-04-07 14:01:09 -0300

    remove modules pixelated-dispatcher is not using

--- modules.go
+++ modules.go
@@ -3,8 +3,5 @@ package main
 import (
 	_ "github.com/gliderlabs/logspout/adapters/raw"
 	_ "github.com/gliderlabs/logspout/adapters/syslog"
-	_ "github.com/gliderlabs/logspout/httpstream"
-	_ "github.com/gliderlabs/logspout/routesapi"
-	_ "github.com/gliderlabs/logspout/transports/tcp"
 	_ "github.com/gliderlabs/logspout/transports/udp"
 )
